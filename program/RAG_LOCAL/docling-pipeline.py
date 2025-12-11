51c1d210dc99929787d0dff86576e7bc58b05ba7
Gustav Moritz <gmoritz@kth.se>
2025-12-11 13:45:24 +0100

    document showcasing docling document structure

--- program/RAG_LOCAL/docling-pipeline.py
+++ program/RAG_LOCAL/docling-pipeline.py
@@ -1,7 +1,3 @@
-"""
-text="Max Emilian Verstappen (Dutch pronunciation: [ˈmɑks fɛrˈstɑpə(n)]; born 30 September 1997) is a Dutch and Belgian racing driver who competes under the Dutch flag in Formula One for Red Bull Racing. Verstappen has won four Formula One World Drivers' Championship titles, which he won consecutively from 2021 to 2024 with Red Bull, and has won 70 Grands Prix across 11 seasons." 
-meta=DocMeta(schema_name='docling_core.transforms.chunker.DocMeta', version='1.0.0', doc_items=[DocItem(self_ref='#/texts/0', parent=RefItem(cref='#/body'), children=[], content_layer=<ContentLayer.BODY: 'body'>, meta=None, label=<DocItemLabel.TEXT: 'text'>, prov=[ProvenanceItem(page_no=1, bbox=BoundingBox(l=72.0, t=761.334, r=509.596, b=687.259, coord_origin=<CoordOrigin.BOTTOMLEFT: 'BOTTOMLEFT'>), charspan=(0, 375))])], headings=None, captions=None, origin=DocumentOrigin(mimetype='application/pdf', binary_hash=15723405278806470466, filename='Max Test.pdf', uri=None))
-"""
 from docling.datamodel.base_models import InputFormat
 from docling.datamodel.pipeline_options import PdfPipelineOptions
 from docling.document_converter import DocumentConverter, PdfFormatOption
